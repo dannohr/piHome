f90b809b0194afb890e1255f0fdbb35ceb8440b8
Dan Nohr <danielnohr@gmail.com>
2019-05-01 09:54:11 -0500

    update

--- api/api-piHome.js
+++ api/api-piHome.js
@@ -1,13 +1,22 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sqlite3 = require("sqlite3").verbose();
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 const port = process.env.PORT || 3001;
+
+// open the database
+let db = new sqlite3.Database("./db/piHome.db", sqlite3.OPEN_READWRITE, err => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the piHome database.");
+});
 
 app.use(logger("dev"));
 app.use(express.json());
