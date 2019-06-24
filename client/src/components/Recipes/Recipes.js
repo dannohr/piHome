691cefd097d0b36d8fecafe28e93dc6bcf925cdc
Dan Nohr <danielnohr@gmail.com>
2019-06-24 18:34:37 -0500

    Beginnings of custom ticker

--- client/src/components/Recipes/Recipes.js
+++ client/src/components/Recipes/Recipes.js
@@ -1,7 +1,9 @@
 import React from "react";
+import StockTicker from "../StockTIcker/StockTicker";
 class Recipes extends React.Component {
   render() {
-    return <h1>Recipes</h1>;
+    // return <h1>Recipes</h1>;
+    return <StockTicker />;
   }
 }
 export default Recipes;
