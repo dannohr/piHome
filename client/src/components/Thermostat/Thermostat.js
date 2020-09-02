01767f7864f05688e81451502eee21ffc9aab839
Dan Nohr <danielnohr@gmail.com>
2020-09-02 15:34:15 -0500

    test

--- client/src/components/Thermostat/Thermostat.js
+++ client/src/components/Thermostat/Thermostat.js
@@ -1,14 +1,13 @@
 import React from "react";
-
 import "./Thermostat.css";
 
-class Thermostat extends React.Component {
-  render() {
-    return (
-      <div className="Wrapper">
-        <h1>Thermostat</h1>
-      </div>
-    );
-  }
+function Thermostat() {
+  return (
+    <div className="wrapper">
+      <div className="currentTemp">Indoor Temperature</div>
+      <div className="targetTemp">Target Temperature</div>
+    </div>
+  );
 }
+
 export default Thermostat;
